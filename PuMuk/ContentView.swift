06fa2e9291bd9918158433ad9717530e0b193dc7
Patrick Sinaga <sinagapatrick610@gmail.com>
2025-11-01 09:09:33 +0700

    Initial Commit

---
+++ PuMuk/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PuMuk
+//
+//  Created by Patrick Samuel Owen Saritua Sinaga on 01/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PuMuk/PuMukApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuMukApp.swift
+//  PuMuk
+//
+//  Created by Patrick Samuel Owen Saritua Sinaga on 01/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PuMukApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
